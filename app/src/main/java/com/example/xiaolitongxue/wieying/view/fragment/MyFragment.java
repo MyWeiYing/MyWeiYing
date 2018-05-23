34a69ffee3d6c363d80c969f8ebf4ffc010456bf
xuhaiyan <haiyan.xu.vip@gmail.com>
2018-05-23 08:41:12 +0800

    lyh

--- app/src/main/java/com/example/xiaolitongxue/wieying/view/fragment/MyFragment.java
+++ app/src/main/java/com/example/xiaolitongxue/wieying/view/fragment/MyFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -56,6 +57,7 @@ public class MyFragment extends BaseFragment<MyPresenter>{
         }
         LinearLayoutManager manager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
         myRecycler.setLayoutManager(manager);
+        myRecycler.addItemDecoration(new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL));
         myRecycler.setAdapter(new MyRecyclerAdapter(list,getContext()));
     }
 
